ad7750922777b8270a51e73bb467f300ff38f29f
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-21 15:39:03 +0800

    Perform Session-2-task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Images } from './assets';
+import { GroceryList } from './GroceryList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    name: 'Abel M. Lugtu Jr',
+    age: 26,
+    DOB: '09-06-1996',
+    salary: 10000,
+    designation: 'MS Tech Solution Trainee',
+    image: Images.catImage,
+    groceryData: [
+      {
+        id: 1,
+        groceryLabel: 'vegetables',
+        description: 'vegetables description',
+      },
+      { id: 2, groceryLabel: 'fruits', description: 'fruits description' },
+      { id: 3, groceryLabel: 'drinks', description: 'drinks description' },
+    ],
+  };
+
+  handleMouseEnter = () => {
+    alert(
+      `${this.state.name}\n${this.state.age}\n${this.state.DOB}\n${this.state.salary}\n${this.state.designation}\n`
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <img alt="Person" className="profile-image" src={this.state.image} />
+        <h1 className="name" onMouseEnter={this.handleMouseEnter}>
+          {this.state.name}
+        </h1>
+        <p>{this.state.designation}</p>
+        <GroceryList groceryData={this.state.groceryData} />
+      </div>
+    );
+  }
 }
 
-export default App;
+// function App() {
+
+//   return (
+//     <div className="App">
+//       adwdw
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/GroceryList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const GroceryList = (props) => {
+  return (
+    <div className="grocery-list">
+      <h2>List of Grocery</h2>
+      {props.groceryData.map((data) => (
+        <div className="grocery-item" key={data.id}>
+          <div>{data.groceryLabel}</div>
+          <div>{data.description}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
